4d9cb8dff58945acc2ede0f4ebeacc429c34d35f
Alex Grinman <alex@krypt.co>
2020-05-02 15:32:38 -0400

    musl_build.sh: use stable rust

--- musl_build.sh
+++ musl_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -v "cargo-cache:$HOME/.cargo" -v "$PWD:/volume" --rm -it clux/muslrust:1.44.0-nightly cargo build --bin wormhole_server --release
+docker run -v "cargo-cache:$HOME/.cargo" -v "$PWD:/volume" --rm -it clux/muslrust:stable cargo build --bin wormhole_server --release
 
